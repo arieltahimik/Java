b387a47d9d8fe73077e698389e22b81f388e2ccd
ArielT <157650875+arieltahimik@users.noreply.github.com>
2024-02-13 23:45:37 -0600

    Remove comments on HelloWorld and add it to CommentExample

--- CommentExample.java
+++ CommentExample.java
@@ -1,11 +1,14 @@
 /**
- * The following class shows what a comment would look like in a program.
- */
+* Javadoc comment
+* Public class name and filename must be the same
+* i.e. CommentExample
+*/
 public class CommentExample {
   // I'm a comment inside the class
   public static void main(String[] args) {
     // I'm a comment inside a method
-    System.out.println("This program has comments!");
+    System.out.print("This program has comments");
+    System.out.println("!"); // I'm a comment on same line
   }
   /*
    * This is a multi-line comment.
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,5 @@
-/* WELCOME TO JAVA
- * This is a 
- * Multi-line comment
- */
-
-/**
-* Javadoc comment
-* Class name and file name are the same
-* - HelloWorld
-*/
 public class HelloWorld {
-	// main() method
 	public static void main(String[] args) {
-		// println outputs string in a new line
 		System.out.println("John: Hello World!");
 		System.out.println("World: Hello John!");
 	}
