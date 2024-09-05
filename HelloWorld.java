4478fdd23870314d8c97755d3dc902b8dde8966f
ArielT <157650875+arieltahimik@users.noreply.github.com>
2024-09-04 23:18:12 -0500

    Revert HelloWorld to last commit (updated by mistake)

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("John: Hello World!")
+		System.out.println("John: Hello World!");
+		System.out.println("World: Hello John!");
 	}
 }
